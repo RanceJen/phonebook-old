280848553f0863e52bbd7ee2a9e5cf228fc6cc6d
BodomMoon <BodomMoon@gmail.com>
2018-03-09 02:14:44 +0800

    Update the detail in struck entry to struct detail

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -6,8 +6,7 @@
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
 // #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
+typedef struct __PHONE_BOOK_DETAIL_ENTRY {
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,8 +16,14 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+} detail;
+
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
     struct __PHONE_BOOK_ENTRY *pNext;
+    detail *pDetail;
 } entry;
+
 
 entry *findName(char lastName[], entry *pHead);
 entry *append(char lastName[], entry *e);
