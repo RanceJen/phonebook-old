45984f05425d0d84d35f46f8049f33914ad5939a
BodomMoon <BodomMoon@gmail.com>
2018-03-09 01:55:15 +0800

    Update check performence more accurate and add hash table
         Add CFLAG OPT to identify diffrent action on orig and opt.
         Add BKDR Hash function and use A&(B-1)=A%B on return value
         complete the free block to free memery more completly

--- main.c
+++ main.c
@@ -5,9 +5,13 @@
 #include <assert.h>
 
 #include IMPL
+#define MAX_TABLE_SIZE 1024
 
-#ifdef OPT
+#if OPT == 1
 #define OUT_FILE "opt.txt"
+
+entry *hashTable[MAX_TABLE_SIZE] = {NULL};
+//entry *tableHead[MAX_TABLE_SIZE] = {NULL};
 #else
 #define OUT_FILE "orig.txt"
 #endif
@@ -25,6 +29,18 @@ static double diff_in_second(struct timespec t1, struct timespec t2)
         diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
     }
     return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
+}
+
+unsigned int BKDRhash(char *cPtr)
+{
+    int hash = 0;
+    int seed = 31; 
+    do
+    {
+        hash = hash * seed + *cPtr;
+        cPtr++;
+    }while(*cPtr != '\0');
+    return (hash & (MAX_TABLE_SIZE -1)); //we learn from class
 }
 
 int main(int argc, char *argv[])
@@ -54,28 +70,43 @@ int main(int argc, char *argv[])
 #endif
     clock_gettime(CLOCK_REALTIME, &start);
     while (fgets(line, sizeof(line), fp)) {
-        line[strlen(line) -1] = '\0';//改成strlen會不會比較好？ 能不能用位移+＆運算來判斷？
+        line[strlen(line) -1] = '\0'; //change while loop to strlen
+#if OPT == 1 //compilering OPT version
+        unsigned int hashValue = BKDRhash(line);
+        if(hashTable[hashValue] == NULL){
+            hashTable[hashValue] =(entry*) malloc(sizeof(entry)); 
+        }
+        append(line , hashTable[hashValue]);  
+#else       //compilering orgin version  
         e = append(line, e);
+#endif
     }
     clock_gettime(CLOCK_REALTIME, &end);
     cpu_time1 = diff_in_second(start, end);
 
     /* close file as soon as possible */
     fclose(fp);
-
     /* the givn last name to find */
     char input[MAX_LAST_NAME_SIZE] = "zyxel";
+
+#if OPT == 1 //compilering OPT version
+    e = hashTable[BKDRhash(input)];
+#else
     e = pHead;
+#endif
 
     assert(findName(input, e) &&
            "Did you implement findName() in " IMPL "?");
     assert(0 == strcmp(findName(input, e)->lastName, "zyxel"));
+
+
 
 #if defined(__GNUC__)
     __builtin___clear_cache((char *) pHead, (char *) pHead + sizeof(entry));
 #endif
     /* compute the execution time */
     clock_gettime(CLOCK_REALTIME, &start);
+    //printf("%s",findName(input, e)->lastName );
     findName(input, e);
     clock_gettime(CLOCK_REALTIME, &end);
     cpu_time2 = diff_in_second(start, end);
@@ -87,8 +118,23 @@ int main(int argc, char *argv[])
     printf("execution time of append() : %lf sec\n", cpu_time1);
     printf("execution time of findName() : %lf sec\n", cpu_time2);
 
-    if (pHead->pNext) free(pHead->pNext);
-    free(pHead);
+#if OPT == 1 //compilering OPT version
+    int counter = 0;
+    do
+    {
+        free(hashTable[counter] -> pNext);
+        free(hashTable[counter] );
+        counter++;
+
+    }while(counter < MAX_TABLE_SIZE);
+#else
+    if (pHead != NULL) {
+        free(pHead->pNext);
+        free(pHead);
+    }
+#endif
 
     return 0;
 }
+
+
